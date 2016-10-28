b5717ecdead7eca3b6f3e593028d4976fba212a5
Richard See <5see@informatik.uni-hamburg.de>
2016-10-28 18:09:34 +0200

    Added Reward not working

--- NeuralNetworksTS/main.py
+++ NeuralNetworksTS/main.py
@@ -3,46 +3,55 @@ import numpy as np
 import random
 import gym
 
+eta = 0.1
+gamma = 0.9
+
+
+
+
+def getDeltaValue(state, actualQValue, reward, npArray):
+   return reward + gamma * npArray[np.argmax(npArray[:, state]), state] - actualQValue
+
 
 def main():
 
+    # Init Q Values to zero
     env = gym.make('FrozenLake-v0')
     env.__init__(is_slippery=False)
-
-    obsersvation = env.reset()
-
-    # Init Q Values to zero
     Q = np.zeros((env.action_space.n, env.observation_space.n))
-
-    Q[3, 1] = 2
-    Q[3, 15] = 9
-    print(Q[:,:])
+    print(Q[:, :])
 
 
 
-    for _ in range(1):
-        for x in range(10):
-            print("Durchgang: ", x)
+    for _ in range(100000):
+        obsersvation = env.reset()
+        for x in range(10000):
+            #print("Durchgang: ", x)
+
 
             if (random.uniform(0, 1) < 0.25):
                 next_action = random.randint(0, 3)
-                print("Is Random!")
+                #print("Is Random!")
 
             else:
                 next_action = np.argmax(Q[:, obsersvation])
 
+
+
+
+
             print("Observation: ", obsersvation)
             print("Next Action Index: ", next_action)
 
-            env.render()
+            #env.render()
             print("-----------------\n")
 
             obsersvation, reward, done, info = env.step(next_action)
-
+            print("Delta: ", getDeltaValue(obsersvation, Q[next_action,obsersvation],reward, Q))
+            Q[next_action, obsersvation] = eta * getDeltaValue(obsersvation, Q[next_action, obsersvation], reward,  Q)
 
             if done:
                 env.reset()
-                #update Q Values
                 break
 
 
